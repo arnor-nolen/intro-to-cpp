3fcb55ba20e75649279dc78bff24819a98e01ff5
Andrei Gorbulin <andrey1gorbulin@gmail.com>
2019-07-01 23:06:29 +0300

    Re-wrote task 6 for better visibility

--- task_6_rotating/main.cpp
+++ task_6_rotating/main.cpp
@@ -1,27 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void rotate_by_one(int &a, unsigned size)
+void rotate_by_one(int a[], unsigned size)
 {
-    cout << &a << endl;
-    cout << *(&a + 3) << endl;
     int last;
     for (int i = 0; i < size; i++)
     {
         int address = i - 1;
         address = address >= 0 ? address : address + size;
         if (i == 0)
-            last = *(&a + size - 1);
-        *(&a + address) = *(&a + i);
+            last = a[size - 1];
+        a[address] = a[i];
         if (i == size - 1)
-            *(&a + size - 2) = last;
+            a[size - 2] = last;
     }
     return;
 }
 
-void rotate(int &a, unsigned size, int shift)
+void rotate(int a[], unsigned size, int shift)
 {
-    cout << &a << endl;
     int real_shift = shift % size;
     if (real_shift == 0)
         return;
@@ -34,8 +31,7 @@ int main()
 {
     const int size = 5, shift = 3;
     int a[size] = {1, 2, 3, 4, 5};
-    cout << &a << endl;
-    rotate(*a, size, shift);
+    rotate(a, size, shift);
     for (int i = 0; i < size; i++)
         cout << a[i] << " ";
     cout << endl;
