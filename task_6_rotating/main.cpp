eb77b338bcaad6b915a97fc01e9a7275b3b50106
Andrei Gorbulin <andrey1gorbulin@gmail.com>
2019-07-01 23:01:39 +0300

    Solved task 6

--- task_6_rotating/main.cpp
+++ task_6_rotating/main.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 using namespace std;
 
-void rotate_by_one(int a[], unsigned size)
+void rotate_by_one(int &a, unsigned size)
 {
+    cout << &a << endl;
+    cout << *(&a + 3) << endl;
     int last;
     for (int i = 0; i < size; i++)
     {
-        int address = i - 2;
+        int address = i - 1;
         address = address >= 0 ? address : address + size;
         if (i == 0)
-            last = a[size - 1];
-        a[address] = a[i];
+            last = *(&a + size - 1);
+        *(&a + address) = *(&a + i);
         if (i == size - 1)
-            a[size - 2] = last;
+            *(&a + size - 2) = last;
     }
     return;
 }
 
-void rotate(int a[], unsigned size, int shift)
+void rotate(int &a, unsigned size, int shift)
 {
+    cout << &a << endl;
     int real_shift = shift % size;
     if (real_shift == 0)
         return;
@@ -29,9 +32,10 @@ void rotate(int a[], unsigned size, int shift)
 
 int main()
 {
-    const int size = 4, shift = 2;
-    int a[size] = {1, 2, 3, 4};
-    rotate(a, size, shift);
+    const int size = 5, shift = 3;
+    int a[size] = {1, 2, 3, 4, 5};
+    cout << &a << endl;
+    rotate(*a, size, shift);
     for (int i = 0; i < size; i++)
         cout << a[i] << " ";
     cout << endl;
