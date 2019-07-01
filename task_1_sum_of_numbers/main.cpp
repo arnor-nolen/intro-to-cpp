49e47aa003e1a52f8f6520891a29f855cae25096
Andrei Gorbulin <andrey1gorbulin@gmail.com>
2019-07-01 09:44:27 +0300

    VS Code config files for build and run

--- task_1_sum_of_numbers/main.cpp
+++ task_1_sum_of_numbers/main.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main()
 {
-    // put your code here
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << x + y << endl;
+    }
 
     return 0;
 }
