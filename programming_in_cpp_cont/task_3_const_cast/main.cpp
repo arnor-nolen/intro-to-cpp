33b797b8b145ff085bd0ef2079c25f629469035c
Andrei Gorbulin <andrey1gorbulin@gmail.com>
2019-07-23 17:43:09 +0300

    Task 3 finished

---
+++ programming_in_cpp_cont/task_3_const_cast/main.cpp
@@ -0,0 +1,14 @@
+int main() {
+  int a = 27;
+  int const b = 412;
+  int *pa = &a;
+
+  int const c = a;
+  int d = b;
+  int const *p1 = pa;
+  int *const *p2 = &pa;
+  int const **p3 = const_cast<int const **>(&pa);
+  int const *const *p4 = &pa;
+
+  return 0;
+}
