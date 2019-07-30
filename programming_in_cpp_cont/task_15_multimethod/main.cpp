f7ca7a8e0af8caa64cc269fbf1e91a49ea02b124
Andrei Gorbulin <andrey1gorbulin@gmail.com>
2019-07-30 16:21:54 +0300

    Task 15 is still in progress

--- programming_in_cpp_cont/task_15_multimethod/main.cpp
+++ programming_in_cpp_cont/task_15_multimethod/main.cpp
@@ -1,75 +1,103 @@
+#include <functional>
 #include <iostream>
+#include <map>
 #include <typeindex>
 #include <typeinfo>
 
-struct Shape {};
-
+struct Shape {
+  virtual void s() {}
+  virtual ~Shape() {}
+};
 struct Rectangle : Shape {};
-
 struct Triangle : Shape {};
 
-bool is_intersect_r_r(Shape* a, Shape* b);
+bool is_intersect_r_r(Shape* a, Shape* b) { return true; };
+bool is_intersect_r_t(Shape* a, Shape* b) { return true; };
 
-bool is_intersect_r_t(Shape* a, Shape* b);
-
-// Base - базовый класс иерархии
-// Result - тип возвращаемого значения мультиметода
-// Commutative - флаг, который показывает, что
-// мультиметод коммутативный (т.е. f(x,y) = f(y,x)).
 template <class Base, class Result, bool Commutative>
 struct Multimethod2 {
-  // устанавливает реализацию мультиметода
-  // для типов t1 и t2 заданных через typeid
-  // f - это функция или функциональный объект
-  // принимающий два указателя на Base
-  // и возвращающий значение типа Result
-  void addImpl(... t1, ... t2, ... f) {}
-
-  // проверяет, есть ли реализация мультиметода
-  // для типов объектов a и b
-  bool hasImpl(Base* a, Base* b) const {
-    // возвращает true, если реализация есть
-    // если операция коммутативная, то нужно
-    // проверить есть ли реализация для b и а
-    return ...;
+  void addImpl(std::type_index t1, std::type_index t2,
+               std::function<Result(Base* a, Base* b)> f) {
+    map_.emplace(std::make_pair(t1, t2), f);
+    std::cout << t1.name() << " " << t2.name() << std::endl;
   }
 
-  // Применяет мультиметод к объектам
-  // по указателям a и b
+  bool hasImpl(Base* a, Base* b) const {
+    auto pair =
+        std::make_pair(std::type_index(typeid(a)), std::type_index(typeid(b)));
+    std::cout << pair.first.name() << " " << pair.second.name() << std::endl;
+    bool swapped = false;
+    if (findImpl(a, b, swapped) != map_.end()) return true;
+    return false;
+  }
+
   Result call(Base* a, Base* b) const {
-    // возвращает результат применения реализации
-    // мультиметода к a и b
-    return ...;
+    bool swapped = false;
+    auto func = findImpl(a, b, swapped)->second;
+    if (!swapped) return func(a, b);
+    return func(b, a);
+  }
+
+ private:
+  std::map<std::pair<std::type_index, std::type_index>,
+           std::function<Result(Base* a, Base* b)>>
+      map_;
+
+  auto findImpl(Base* a, Base* b, bool& swapped) const -> decltype(map_.end()) {
+    swapped = false;
+    auto pair =
+        std::make_pair(std::type_index(typeid(a)), std::type_index(typeid(b)));
+    auto pair_commutative =
+        std::make_pair(std::type_index(typeid(b)), std::type_index(typeid(a)));
+    std::cout << pair.first.name() << " " << pair.second.name() << std::endl;
+    auto entry = map_.find(pair);
+    auto entry_commutative = map_.find(pair_commutative);
+    auto end = map_.end();
+    if (entry != end) {
+      return entry;
+    } else if (Commutative) {
+      if (entry_commutative != end) {
+        swapped = true;
+        return entry_commutative;
+      }
+    }
+    return map_.end();
   }
 };
 
 int main() {
-  // мультиметод для наследников Shape
-  // возращающий bool и являющийся коммутативным
-  Multimethod2<Shape, bool, true> is_intersect;
-
-  // добавляем реализацию мультиметода для двух прямоугольников
-  is_intersect.addImpl(typeid(Rectangle), typeid(Rectangle), is_intersect_r_r);
-
-  // добавляем реализацию мультиметода для прямоугольника и треугольника
-  is_intersect.addImpl(typeid(Rectangle), typeid(Triangle), is_intersect_r_t);
-
-  // создаём две фигуры
   Shape* s1 = new Triangle();
   Shape* s2 = new Rectangle();
+  Triangle* s3 = new Triangle();
 
-  // проверяем, что реализация для s1 и s2 есть
-  if (is_intersect.hasImpl(s1, s2)) {
-    // вызывается функция is_intersect_r_t(s2, s1)
-    bool res = is_intersect.call(s1, s2);
+  std::cout << typeid(s1).name() << " " << typeid(s2).name() << " "
+            << typeid(s3).name() << std::endl;
 
-    // Замечание: is_intersect_r_t ожидает,
-    // что первым аргументом будет прямоугольник
-    // а вторым треугольник, а здесь аргументы
-    // передаются в обратном порядке.
-    // ваша реализация должна самостоятельно
-    // об этом позаботиться
+  //------
+  // std::map<std::pair<std::type_index, std::type_index>,
+  //          std::function<bool(Shape*, Shape*)>>
+  //     mymap;
+
+  // mymap.emplace(
+  //     std::make_pair(std::type_index(typeid(s1)),
+  //     std::type_index(typeid(s2))), is_intersect_r_t);
+  // auto l = mymap.find(
+  //     std::make_pair(std::type_index(typeid(s1)),
+  //     std::type_index(typeid(s2))));
+  // std::cout << l->second(s2, s1) << std::endl;
+  //------
+
+  Multimethod2<Shape, bool, true> is_intersect;
+
+  is_intersect.addImpl(typeid(Rectangle), typeid(Rectangle), is_intersect_r_r);
+  is_intersect.addImpl(typeid(Rectangle), typeid(Triangle), is_intersect_r_t);
+
+  bool intersect = is_intersect.hasImpl(s2, s1);
+  if (intersect) {
+    bool res = is_intersect.call(s2, s1);
+    std::cout << res << std::endl;
   }
+  std::cout << intersect << std::endl;
 
   return 0;
 }
