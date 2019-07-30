7ec343646a02dbb7df7b0d8b04f18f896dad4630
Andrei Gorbulin <andrey1gorbulin@gmail.com>
2019-07-31 00:16:17 +0300

    Task 15 finished

--- programming_in_cpp_cont/task_15_multimethod/main.cpp
+++ programming_in_cpp_cont/task_15_multimethod/main.cpp
@@ -5,53 +5,45 @@
 #include <typeinfo>
 
 struct Shape {
-  virtual void s() {}
-  virtual ~Shape() {}
+  virtual ~Shape(){};
 };
 struct Rectangle : Shape {};
 struct Triangle : Shape {};
 
-bool is_intersect_r_r(Shape* a, Shape* b) { return true; };
-bool is_intersect_r_t(Shape* a, Shape* b) { return true; };
+bool is_intersect_r_r(Shape *a, Shape *b) { return true; };
+bool is_intersect_r_t(Shape *a, Shape *b) { return true; };
 
-template <class Base, class Result, bool Commutative>
-struct Multimethod2 {
+template <class Base, class Result, bool Commutative> struct Multimethod2 {
   void addImpl(std::type_index t1, std::type_index t2,
-               std::function<Result(Base* a, Base* b)> f) {
-    map_.emplace(std::make_pair(t1, t2), f);
-    std::cout << t1.name() << " " << t2.name() << std::endl;
+               std::function<Result(Base *a, Base *b)> f) {
+    map_.insert({{t1, t2}, f});
   }
 
-  bool hasImpl(Base* a, Base* b) const {
-    auto pair =
-        std::make_pair(std::type_index(typeid(a)), std::type_index(typeid(b)));
-    std::cout << pair.first.name() << " " << pair.second.name() << std::endl;
+  bool hasImpl(Base *a, Base *b) const {
+    auto pair = {std::type_index(typeid(*a)), std::type_index(typeid(*b))};
     bool swapped = false;
-    if (findImpl(a, b, swapped) != map_.end()) return true;
+    if (findImpl(a, b, swapped) != map_.end())
+      return true;
     return false;
   }
 
-  Result call(Base* a, Base* b) const {
+  Result call(Base *a, Base *b) const {
     bool swapped = false;
     auto func = findImpl(a, b, swapped)->second;
-    if (!swapped) return func(a, b);
+    if (!swapped)
+      return func(a, b);
     return func(b, a);
   }
 
- private:
+private:
   std::map<std::pair<std::type_index, std::type_index>,
-           std::function<Result(Base* a, Base* b)>>
+           std::function<Result(Base *a, Base *b)>>
       map_;
 
-  auto findImpl(Base* a, Base* b, bool& swapped) const -> decltype(map_.end()) {
+  auto findImpl(Base *a, Base *b, bool &swapped) const -> decltype(map_.end()) {
     swapped = false;
-    auto pair =
-        std::make_pair(std::type_index(typeid(a)), std::type_index(typeid(b)));
-    auto pair_commutative =
-        std::make_pair(std::type_index(typeid(b)), std::type_index(typeid(a)));
-    std::cout << pair.first.name() << " " << pair.second.name() << std::endl;
-    auto entry = map_.find(pair);
-    auto entry_commutative = map_.find(pair_commutative);
+    auto entry = map_.find({typeid(*a), typeid(*b)});
+    auto entry_commutative = map_.find({typeid(*a), typeid(*b)});
     auto end = map_.end();
     if (entry != end) {
       return entry;
@@ -66,26 +58,8 @@ struct Multimethod2 {
 };
 
 int main() {
-  Shape* s1 = new Triangle();
-  Shape* s2 = new Rectangle();
-  Triangle* s3 = new Triangle();
-
-  std::cout << typeid(s1).name() << " " << typeid(s2).name() << " "
-            << typeid(s3).name() << std::endl;
-
-  //------
-  // std::map<std::pair<std::type_index, std::type_index>,
-  //          std::function<bool(Shape*, Shape*)>>
-  //     mymap;
-
-  // mymap.emplace(
-  //     std::make_pair(std::type_index(typeid(s1)),
-  //     std::type_index(typeid(s2))), is_intersect_r_t);
-  // auto l = mymap.find(
-  //     std::make_pair(std::type_index(typeid(s1)),
-  //     std::type_index(typeid(s2))));
-  // std::cout << l->second(s2, s1) << std::endl;
-  //------
+  Shape *s1 = new Triangle();
+  Shape *s2 = new Rectangle();
 
   Multimethod2<Shape, bool, true> is_intersect;
 
